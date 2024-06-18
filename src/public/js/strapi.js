36fcb7c33b3890b11a940691fbd0051d5d6e65b3
NordajC <chongjunhann9@gmail.com>
2024-06-17 18:39:32 -0700

    added script to copy js files into the public folder

---
+++ src/public/js/strapi.js
@@ -0,0 +1,8 @@
+fetch('http://localhost:1337/api/restaurants', {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+  .then(response => response.json())
+  .then(data => console.log(data));
